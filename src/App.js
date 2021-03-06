448d3c89d2ec5a2bf16f2c647f7a1c08c8246afd
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-03-07 03:25:21 +0600

    route added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
 import { useEffect, useState } from 'react';
-import Country from './Competent/Country';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import Home from './Competent/Home/Home';
+ 
 
 function App() {
  
-  const [countries, setCountries] =  useState([]);
-  useEffect(() => {
-    fetch('https://restcountries.eu/rest/v2/all')
-    .then(res => res.json())
-    .then(data => setCountries(data))
-  },[] )
+ 
   return (
     <div>
-      <h1>Countries Number : {countries.length}</h1>
-      {
-        countries.map(country => <Country country={country}> </Country>)
-       
-      }
+     <Home></Home>
        
     </div>
   );
