ae5afb66739b05cce57b682c79736039b89875fb
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-03-07 04:29:20 +0600

    ok before name added

--- src/Competent/CountryDetail/CountryDetail.js
+++ src/Competent/CountryDetail/CountryDetail.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
 const CountryDetail = () => {
     const{countryCallingCodes}= useParams();
+    
+    const [country,setCountry] = useState([]);
+    useEffect(() =>{
+        fetch(`https://restcountries.eu/rest/v2/callingcode/${countryCallingCodes}`)
+        .then(res => res.json())
+        .then(data => setCountry(data));
+    })
     return (
         <div>
-            <h2>This is country detail </h2>
+            <h2>This is country detail: {countryCallingCodes} </h2>
+            
             
         </div>
     );
