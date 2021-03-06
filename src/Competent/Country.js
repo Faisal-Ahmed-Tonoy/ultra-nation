d138edbe1ada76383baa1cb762e8d1b3178dcb29
Faisal-Ahmed-Tonoy <71918750+Faisal-Ahmed-Tonoy@users.noreply.github.com>
2021-03-07 03:06:23 +0600

    fourth commit

--- src/Competent/Country.js
+++ src/Competent/Country.js
@@ -3,13 +3,15 @@ import React from 'react';
 const Country = (props) => {
     const {name,capital,population} =props.country;
     const countryStyle ={
+        float:'left',
         border:'1px solid purple',
         margin:'20px',
         padding:'20px',
-        borderRadius:'20px'
+        borderRadius:'20px',
+        width:'25.25%'
     }
     return (
-        <div>
+        <div style={countryStyle}>
             <h1>Country Name :{name}</h1>
             <h2> Capital:{capital}</h2>
             <p>Population:{population}</p>
